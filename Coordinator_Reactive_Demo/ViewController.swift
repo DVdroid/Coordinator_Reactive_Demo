7b48b4f14598ac31c955f338b7e6a600e4d224e9
Vikash Anand <anandin02@gmail.com>
2021-08-21 18:15:44 +0530

    Initial Commit

---
+++ Coordinator_Reactive_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator_Reactive_Demo
+//
+//  Created by Vikash Anand on 21/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coordinator_Reactive_DemoTests/Coordinator_Reactive_DemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Coordinator_Reactive_DemoTests.swift
+//  Coordinator_Reactive_DemoTests
+//
+//  Created by Vikash Anand on 21/08/21.
+//
+
+import XCTest
+@testable import Coordinator_Reactive_Demo
+
+class Coordinator_Reactive_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
